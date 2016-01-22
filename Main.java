950ccce5c2e797e906bf4e569c9110f7fccbf41d
IGI-111 <igi.222@gmail.com>
2016-01-22 16:29:50 +0100

    changed display and tentative generation

--- Main.java
+++ Main.java
@@ -1,28 +1,39 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.HashSet;
+import java.util.TreeSet;
 import java.util.List;
+import java.util.Arrays;
 import java.util.Collection;
 
 public class Main {
     public static void main(String[] args) {
         // add collections to test here
         List<Collection<String>> toBench = new ArrayList<Collection<String>>();
-        toBench.add(new ArrayList<String>());
-        toBench.add(new LinkedList<String>());
+        toBench.add(new ArrayList<String>());  // Array
+        toBench.add(new LinkedList<String>()); // List
+        toBench.add(new TreeSet<String>());    // AVL
+        toBench.add(new HashSet<String>());    // Hash table
 
-        //TODO: generate dataset
-        List<String> dataset = new ArrayList<String>();
-        for (int i = 0; i < 100; i++) {
-            dataset.add("test1");
-        }
+        for (int dataSize = 1; dataSize <= 6; ++dataSize) {
+            System.out.println("Dataset size: " + dataSize);
 
-        for (Collection<String> c : toBench) {
-            Benchmark<String> b = new Benchmark<String>(c, dataset);
-            Benchmark.Result result = b.bench();
-            System.out.println(result.collection + " " +
-                    " Insert: " + result.insertTime +
-                    " Delete: " + result.deleteTime +
-                    " Search: " + result.searchTime);
+            //TODO: generate dataset
+            List<String> dataset = new ArrayList<String>();
+            for (int i = 0; i < Math.pow(26, dataSize); i++) {
+                dataset.add("dummy"); //FIXME
+            }
+
+            for (Collection<String> c : toBench) {
+                Benchmark<String> b = new Benchmark<String>(c, dataset);
+                Benchmark.Result result = b.bench();
+                System.out.println(
+                        result.collection +
+                         " Insert: " + result.insertTime +
+                         " Remove: " + result.deleteTime +
+                         " Find: " + result.searchTime);
+            }
+            System.out.println();
         }
     }
 }
