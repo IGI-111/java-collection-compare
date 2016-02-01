1e3a08a358225f44b9725e1e864344483dfed6c2
IGI-111 <igi.222@gmail.com>
2016-02-02 00:28:35 +0100

    Advanced on AVL insertion; deletion still to do

--- AVLTree.java
+++ AVLTree.java
@@ -1,4 +1,4 @@
-
+import java.util.Stack;
 import java.util.Collection;
 
 public class AVLTree<E extends Comparable<E>>
@@ -13,9 +13,97 @@ public class AVLTree<E extends Comparable<E>>
     }
 
     public boolean add(E element) {
-        return addAVL(root, null, true, element);
-    }
-
+		Stack<WeightedNode<E>> stack = new Stack<WeightedNode<E>>();
+		for ( WeightedNode<E> current = root ; current != null ; stack.push(current)) {
+			if (current.val.compareTo(element) <= 0) {
+				current = current.left;
+			} else {
+				current = current.left;
+			}
+		} 
+		WeightedNode<E> insertAt = stack.pop();
+		stack.push(insertAt);
+		if (insertAt.val.compareTo(element) <= 0) {
+			insertAt.right = new WeightedNode<E>(null, null, element, 0);
+		} else {
+			insertAt.left = new WeightedNode<E>(null, null, element, 0);
+		}
+		WeightedNode<E> parent = null;
+		for (WeightedNode<E> current = root ; !stack.empty() ; current = stack.pop()) {
+			
+			current.weight = (current.left == null ? 0 : current.left.weight) 
+			                 - (current.right == null ? 0 : current.right.weight);
+			if (parent == null && (current.weight > 1 || current.weight < 1)) {
+				parent = current;
+			}
+		}
+		WeightedNode<E> child;
+		WeightedNode<E> parentBis;
+		
+		if (parent.left == null) { 
+			child = parent.right;
+		} else if (parent.right == null) {
+			child = parent.left;
+		} else {
+			child = parent.left.weight > parent.right.weight ? parent.left : parent.right;
+		}
+		do {
+			parentBis = parent;
+			if (child == parent.left) {
+				if (parent.weight == 2) {
+					if (child.weight == -1) {
+						parentBis = child;
+						child = rotateLeft(child);
+					}
+					parent = rotateRight(parent);
+					break;
+				}
+				if (parent.weight == -1) {
+					parent.weight = 0;
+					break;
+				}
+				parent.weight = 1;
+			} else {
+				if (parent.weight == -2) {
+					if(child.weight == 1) {
+						parentBis = child;
+						child = rotateRight(child);
+					}
+					parent = rotateRight(parent);
+					break;
+				}
+				if (parent.weight == 1) {
+					parent.weight = 0;
+					break;
+				}
+				parent.weight = -1;
+			}
+			child = parent;
+			parent = parentBis;
+		} while(parent != null);
+		return true;
+	}
+	
+	public WeightedNode<E> rotateLeft(WeightedNode<E> node) {
+		WeightedNode<E> newRoot = node.right;
+		WeightedNode<E> newRootLeft = node;
+		if (newRoot.left != null) {
+			newRootLeft.right = newRoot.left;
+		}
+		newRoot.left = newRootLeft;
+		return newRoot;
+	}
+	
+	public WeightedNode<E> rotateRight(WeightedNode<E> node) {
+		WeightedNode<E> newRoot = node.left;
+		WeightedNode<E> newRootRight = node;
+		if (newRoot.right != null) {
+			newRootRight.left = newRoot.right;
+		}
+		newRoot.right = newRootRight;
+		return newRoot;
+	}
+	/*
     private boolean addAVL(
         WeightedNode<E> r, WeightedNode<E> p, boolean g, E e) {
         if (r == null) {
@@ -139,10 +227,107 @@ public class AVLTree<E extends Comparable<E>>
                 r.weight = 0;
                 return false;
         }
-    }
+    } 
+    */
 
     public boolean remove(Object element) {
         // TODO
         return false;
     }
+   
+    /*
+        WeightedNode<E> a, aa, p, pp;
+		WeightedNode<E> y = new WeightedNode<E>(null, null, element, 0);
+		WeightedNode<E> r = root; 
+		
+		if (root == null) {
+			root = y;
+		}
+		else {
+			a = root;
+			aa = null;
+			p = root; 
+			pp = null;
+			
+			while (p != null) {
+				if (p.weight != 0) {
+					a = p;
+					r = a;
+					aa = pp;
+				}
+				pp = p;
+				p = y.val.compareTo(y.val) <= 0 ? p.left : p.right;
+			}
+
+			if (y.val.compareTo(pp.val) <= 0) {
+				pp.left = y;
+			} else {
+				pp.right = y;
+			}
+			p = a;
+
+			while ( p != y ) {
+				if (y.val.compareTo(p.val) <= 0) {
+					++p.weight;
+					p = p.left;
+				} else {
+					--p.weight;
+					p = p.right;
+				}
+			}
+			
+			if(a.weight == -2) {
+				if (a.right.weight == -1) {
+					r = rotateLeft(a);
+					r.weight = 0;
+					r.left.weight = 0;
+				}
+				else if(a.right.weight==1) {
+					r = rotateRightLeft(a);
+					if (r.weight == 1)	{
+						r.left.weight = 0;
+						r.right.weight = -1;
+					} else if (r.weight == -1) {
+						r.left.weight = 1;
+						r.right.weight = 0;
+							
+					} else if (r.weight == 0) {
+						r.left.weight = 0;
+						r.right.weight = 0;
+					}
+					r.weight = 0;
+				}
+			}
+						
+			if(a.weight == 2)	{
+				if(a.left.weight == 1) {	
+					r = rotateRight(a);
+					r.weight = 0;
+					r.right.weight = 0;
+				}
+				else if(a.left.weight == -1) {	
+					r = rotateLeftRight(a);	
+					if (r.weight==1)	{
+						r.left.weight = 1;
+						r.right.weight = 0;
+					} else if (r.weight == -1) {
+						r.left.weight = 0;
+						r.right.weight = -1;
+					} else if (r.weight == 0) {
+						r.left.weight = 0;
+						r.right.weight = 0;
+					}
+					r.weight = 0;
+				}
+			}
+			
+			if(aa == null) root = r;
+			else if(aa.val.compareTo(r.val) >= 0) aa.left = r;
+			else aa.right = r;
+			++size;
+			return true;	
+		}
+		return false;
+	}
+	*/
 }
--- WeightedNode.java
+++ WeightedNode.java
@@ -1,9 +1,11 @@
 public class WeightedNode<E extends Comparable<E>>
     extends Node<E, WeightedNode<E>> {
+	
+	public int weight;
+	
     public WeightedNode(
         WeightedNode<E> left, WeightedNode<E> right, E val, int weight) {
         super(left, right, val);
         this.weight = weight;
     }
-    public int weight;
 }
